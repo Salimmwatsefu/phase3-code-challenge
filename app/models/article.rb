0f5432294ff74a8b6b2350258634ffb1954c19e6
Salim Mwatsefu Mwarika <sjmwatsefu@gmail.com>
2022-10-30 22:13:04 +0300

    initialization done

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,25 +6,17 @@ class Article
         @author = author
         @magazine = magazine
         @title = title
+        @@all << self
     end
 
-    def title
-        @title
-    end
 
     def self.all
         @@all
         
     end
 
-    def author
-		@author
-	end
 
-    
-	def magazine
-	 	@magazine
-	end
+
 
 
 end
